a95fc81d435b256eeffe8de70c47dde3ec4bec70
Sebas <sheva3232@gmail.com>
2024-01-18 15:38:49 -0500

    app renamed

--- ios/GhostPayTests/GhostPayTests.m
+++ ios/GhostPayTests/GhostPayTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reactnativestarterTests : XCTestCase
+@interface GhostPayTests : XCTestCase
 
 @end
 
-@implementation reactnativestarterTests
+@implementation GhostPayTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
